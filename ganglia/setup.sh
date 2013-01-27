4fdaf108fc3377a41a5c0727249917da06833dd8
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-01-26 23:53:04 -0800

    Fix a couple of ganglia specific bugs

--- ganglia/setup.sh
+++ ganglia/setup.sh
@@ -9,6 +9,11 @@ for node in $SLAVES $OTHER_MASTERS; do
   ssh -t -t $SSH_OPTS root@$node "/etc/init.d/gmond restart"
 done
 
+# gmeta needs rrds to be owned by nobody
+chown -R nobody /var/lib/ganglia/rrds
+# cluster-wide aggregates only show up with this. TODO: Fix this cleanly ?
+ln -s /usr/share/ganglia/conf/default.json /var/lib/ganglia/conf/
+
 /etc/init.d/gmetad restart
 
 # Start http server to serve ganglia
