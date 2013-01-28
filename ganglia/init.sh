0e7bd66942f951a42fc8c898e64cf37c4af25f8b
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-01-28 13:00:48 -0800

    Try to make ganglia install happen in parallel

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -4,5 +4,6 @@
 # TODO: Remove this once the AMI has ganglia by default
 yum install -q -y ganglia ganglia-gmetad ganglia-gmond ganglia-web
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "yum install -q -y ganglia ganglia-gmetad ganglia-gmond ganglia-web"
+  ssh -t -t $SSH_OPTS root@$node "yum install -q -y ganglia ganglia-gmetad ganglia-gmond ganglia-web" & sleep 0.3
 done
+wait
