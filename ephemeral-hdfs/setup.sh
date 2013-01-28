6dd62d4744db43743786d207e32563f44d7fcebf
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-01-28 13:13:40 -0800

    Don't format namenode if it is already formatted

--- ephemeral-hdfs/setup.sh
+++ ephemeral-hdfs/setup.sh
@@ -9,11 +9,18 @@ for node in $SLAVES $OTHER_MASTERS; do
   echo $node
   ssh -t -t $SSH_OPTS root@$node "/root/spark-ec2/ephemeral-hdfs/setup-slave.sh" & sleep 0.3
 done
+wait
 
 /root/spark-ec2/copy-dir $EPHEMERAL_HDFS/conf
 
-echo "Formatting ephemeral HDFS namenode..."
-$EPHEMERAL_HDFS/bin/hadoop namenode -format
+NAMENODE_DIR=/mnt/ephemeral-hdfs/dfs/name
+
+if [ -f "$NAMENODE_DIR/current/VERSION" ] && [ -f "$NAMENODE_DIR/current/fsimage" ]; then
+  echo "Hadoop namenode appears to be formatted: skipping"
+else
+  echo "Formatting ephemeral HDFS namenode..."
+  $EPHEMERAL_HDFS/bin/hadoop namenode -format
+fi
 
 echo "Starting ephemeral HDFS..."
 $EPHEMERAL_HDFS/bin/start-dfs.sh
