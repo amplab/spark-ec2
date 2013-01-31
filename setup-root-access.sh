9ff84b5ff98deb7d9707999b45f87c799d3511b1
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-01-30 17:52:28 -0800

    Add script to setup root access on ec2 vanilla ami

---
+++ setup-root-access.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum installi -y -q git python dstat strace java-1.6.0-openjdk-devel ant
+
+sudo sed -i 's/PermitRootLogin.*/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+sudo cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/
+sudo /etc/init.d/sshd restart
