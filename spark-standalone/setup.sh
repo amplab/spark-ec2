8ec20341619dda7c8cb01d584825a9bb394787e3
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-01-31 22:09:47 -0800

    Fix cluster url and try a larger sleep duration

--- spark-standalone/setup.sh
+++ spark-standalone/setup.sh
@@ -3,12 +3,12 @@
 cp /root/spark-ec2/slaves /root/spark/conf/
 
 # Set cluster-url to standalone master
-echo "`cat /root/spark-ec2/masters`"":7077" > /root/spark-ec2/cluster-url
+echo "spark://""`cat /root/spark-ec2/masters`"":7077" > /root/spark-ec2/cluster-url
 /root/spark-ec2/copy-dir /root/spark/conf
 
 # The Spark master seems to take time to start and workers crash if
 # they start before the master. Try to see if waiting makes the master start up
-# faster.
-sleep 10
+# more reliably.
+sleep 60
 
 /root/spark/bin/start-all.sh
