26a11eea3b7e76a850f2286e13926727f8da2bc2
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-02-01 11:17:12 -0800

    Check if ganglia is already installed

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -2,8 +2,13 @@
 
 # Install ganglia
 # TODO: Remove this once the AMI has ganglia by default
-yum install -q -y ganglia ganglia-gmetad ganglia-gmond ganglia-web
+
+GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
+
+if ! rpm --quiet -q $GANGLIA_PACKAGES; then
+  yum install -q -y $GANGLIA_PACKAGES;
+fi
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "yum install -q -y ganglia ganglia-gmetad ganglia-gmond ganglia-web" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
 done
 wait
