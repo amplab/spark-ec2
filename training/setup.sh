b1cbebc15871d9197b2d5ee12f0bbfca2f7af4e2
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-02-04 19:32:47 -0800

    Add a couple of training specific requirements

--- training/setup.sh
+++ training/setup.sh
@@ -15,3 +15,18 @@ popd
 
 /root/spark-ec2/copy-dir /root/streaming
 /root/spark-ec2/copy-dir /root/kmeans
+
+# Training specific hacks
+
+# Check if Hadoop version is 0.205.0 in project/SparkBuild.scala
+# if not rebuild spark
+
+HADOOP_VERSION=`cat /root/spark/project/SparkBuild.scala | grep "val HADOOP_VERSION =" | grep -v "//" | awk '{print $NF}' | tr -d \"`
+if [[ "$HADOOP_VERSION" != "0.20.205.0" ]]; then
+  echo "Setting hadoop version to 0.20.205.0 ..."
+  sed -i 's/val HADOOP_VERSION = \"'$HADOOP_VERSION'\"/val HADOOP_VERSION = \"0.20.205.0\"/g' /root/spark/project/SparkBuild.scala
+  pushd /root/spark
+  ./sbt/sbt clean publish-local
+  /root/spark-ec2/copy-dir /root/spark
+  popd
+fi
