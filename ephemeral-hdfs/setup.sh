48bce1da2611c0af06b513e557e024a0c97ee98e
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-02-04 17:34:39 -0800

    Set HDFS_URL which can be used in the AMI

--- ephemeral-hdfs/setup.sh
+++ ephemeral-hdfs/setup.sh
@@ -2,6 +2,10 @@
 
 EPHEMERAL_HDFS=/root/ephemeral-hdfs
 
+# Set hdfs url to make it easier
+HDFS_URL="hdfs://$PUBLIC_DNS:9000"
+echo "export HDFS_URL=$HDFS_URL" >> ~/.bash_profile
+
 pushd /root/spark-ec2/ephemeral-hdfs
 source ./setup-slave.sh
 
