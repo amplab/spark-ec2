aa2ba8a98d056163b2a9897b3cb00d30812aad3d
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-02-18 14:21:03 -0800

    Start spark master and slaves separately. As the JSON health check only works from 0.7.0, use a simpler solution.

--- spark-standalone/setup.sh
+++ spark-standalone/setup.sh
@@ -4,29 +4,30 @@ pushd /root/spark-ec2/spark-standalone
 
 # Copy the slaves to spark conf
 cp /root/spark-ec2/slaves /root/spark/conf/
+/root/spark-ec2/copy-dir /root/spark/conf
 
 # Set cluster-url to standalone master
 echo "spark://""`cat /root/spark-ec2/masters`"":7077" > /root/spark-ec2/cluster-url
 cp -f /root/spark-ec2/cluster-url /root/mesos-ec2/cluster-url
-/root/spark-ec2/copy-dir /root/spark/conf
+/root/spark-ec2/copy-dir /root/spark-ec2
+/root/spark-ec2/copy-dir /root/mesos-ec2
 
 # The Spark master seems to take time to start and workers crash if
-# they start before the master. Retry a few times with a health check
+# they start before the master. So start the master first, sleep and then start
+# workers.
 
-NUM_RETRIES=3
-STATUS=1
-ATTEMPTS=1
+# Stop anything that is running
+/root/spark/bin/stop-all.sh
 
-while [ $STATUS -ne 0 ] && [ $ATTEMPTS -le $NUM_RETRIES ]
-do
-  echo "Starting Spark...Attempt $ATTEMPTS/$NUM_RETRIES" 
-  /root/spark/bin/stop-all.sh
-  sleep 2
-  /root/spark/bin/start-all.sh
-  sleep 5
-  python ./check_spark.py `cat /root/spark-ec2/masters` `wc -l /root/spark/conf/slaves`
-  STATUS=$?
-  ATTEMPTS=$(( $ATTEMPTS + 1 ))
-done
+sleep 2
+
+# Start Master
+/root/spark/bin/start-master.sh
+
+# Pause
+sleep 20
+
+# Start Workers
+/root/spark/bin/start-slaves.sh
 
 popd
