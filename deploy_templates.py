fd767f158f8e22b22e9376b5732a4f511531e221
Patrick Wendell <pwendell@gmail.com>
2013-08-01 11:34:57 -0700

    Small fix from merging

--- deploy_templates.py
+++ deploy_templates.py
@@ -46,7 +46,7 @@ template_vars = {
   "default_spark_mem": "%dm" % spark_mb,
   "spark_version": os.getenv("SPARK_VERSION"),
   "shark_version": os.getenv("SHARK_VERSION"),
-  "hadoop_major_version": os.getenv("HADOOP_MAJOR_VERSION")
+  "hadoop_major_version": os.getenv("HADOOP_MAJOR_VERSION"),
   "scala_home": os.getenv("SCALA_HOME"),
   "java_home": os.getenv("JAVA_HOME")
 }
