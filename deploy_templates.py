f15087a65c1c3cbdc044b488a67dbde2a622fa97
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-10-07 09:36:54 -0700

    Fix a bug in computing memory requirements

--- deploy_templates.py
+++ deploy_templates.py
@@ -26,9 +26,9 @@ system_ram_kb = min(slave_ram_kb, master_ram_kb)
 
 system_ram_mb = system_ram_kb / 1024
 # Leave some RAM for the OS, Hadoop daemons, and system caches
-if system_ram_mb > 100*1024;
+if system_ram_mb > 100*1024:
   spark_mb = system_ram_mb - 15 * 1024 # Leave 15 GB RAM
-if system_ram_mb > 60*1024;
+if system_ram_mb > 60*1024:
   spark_mb = system_ram_mb - 10 * 1024 # Leave 10 GB RAM
 if system_ram_mb > 40*1024:
   spark_mb = system_ram_mb - 6 * 1024 # Leave 6 GB RAM
