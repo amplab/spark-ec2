f2c97ce8b4e7d27ac71052ffc48298789b09730c
Patrick Wendell <pwendell@gmail.com>
2013-12-01 19:22:51 -0800

    Removing MLI/BDB/Mesos stuff

--- training/setup.sh
+++ training/setup.sh
@@ -17,66 +17,12 @@ pushd training
 /root/spark-ec2/copy-dir /root/training
 
 ln -T -f -s /root/training/streaming /root/streaming
-#ln -T -f -s /root/training/kmeans /root/kmeans
+ln -T -f -s /root/training/kmeans /root/kmeans
 ln -T -f -s /root/training/java-app-template /root/java-app-template
 ln -T -f -s /root/training/scala-app-template /root/scala-app-template
-
-# DRY RUN HACK
-# Copy spark-env.sh and slave to 0.7.1 from master
-#cp /root/spark/conf/slaves /root/spark-0.7.1/conf/
-#cp /root/spark/conf/spark-env.sh /root/spark-0.7.1/conf/
-#/root/spark-ec2/copy-dir /root/spark-0.7.1/conf
 
 # Add hdfs to the classpath
 cp /root/ephemeral-hdfs/conf/core-site.xml /root/spark/conf/
 popd
-
-# Build MLI assembly
-#
-pushd /root/MLI
-git remote set-url origin https://github.com/amplab/MLI.git
-git pull
-# NOTE: This is the commit from Aug 28, 2013 that was used during AMP Camp 3
-git checkout -b ampcamp3 a238c6ed96a78d349e274397b835d72c3a60ad94
-./sbt/sbt assembly
-/root/spark-ec2/copy-dir /root/MLI
-popd
-
-# Pull and rebuild blinkdb
-pushd /root/hive_blinkdb
-git pull
-ant package
-/root/spark-ec2/copy-dir /root/hive_blinkdb
-popd
-
-pushd /root/blinkdb
-#git fetch --all
-#git checkout origin/alpha-0.1.0
-#git checkout -b alpha-0.1.0
-git pull
-# NOTE: This is the commit from Aug 27 2013 that was used for AMP Camp 3
-git checkout -b ampcamp3 f843491084777e4af1cd12cc9cf4b585118cbd30
-./sbt/sbt clean package
-
-# Uncomment to make blinkdb use Spark 0.7.1
-# sed -i 's/export SPARK_HOME.*/export SPARK_HOME=\"\/root\/spark-0.7.1\"/g conf/shark-env.sh
-
-/root/spark-ec2/copy-dir /root/blinkdb
-popd
-
-# Tar and copy hadoop, spark
-pushd /root
-echo "Copying Hadoop executor for Mesos"
-tar czf /mnt/ephemeral-hdfs.tar.gz ephemeral-hdfs
-~/ephemeral-hdfs/bin/hadoop fs -put /mnt/ephemeral-hdfs.tar.gz /
-echo "Copying Spark executor for Mesos"
-tar czf /mnt/spark.tar.gz spark
-~/ephemeral-hdfs/bin/hadoop fs -put /mnt/spark.tar.gz /
-rm /mnt/spark.tar.gz
-rm /mnt/ephemeral-hdfs.tar.gz
-popd
-
-#echo "Starting Hadoop Job tracker on Mesos"
-#nohup ~/ephemeral-hdfs/bin/hadoop jobtracker 2>&1 >/mnt/job-tracker.out &
 
 popd
