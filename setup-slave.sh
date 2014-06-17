9a60da167f8611e9d8cbda4ecd8d58726f3526a9
Reynold Xin <rxin@apache.org>
2014-06-17 15:28:35 -0700

    Added a line of comment about TRIM support.

--- setup-slave.sh
+++ setup-slave.sh
@@ -22,12 +22,14 @@ if [[ $instance_type == r3* ]]; then
   EXT4_MOUNT_OPTS="defaults,noatime,nodiratime"
   rm -rf /mnt*
   mkdir /mnt
+  # To turn TRIM support on, uncomment the following line.
   #echo '/dev/sdb /mnt  ext4  defaults,noatime,nodiratime,discard 0 0' >> /etc/fstab
   mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/sdb
   mount -o $EXT4_MOUNT_OPTS /dev/sdb /mnt
 
   if [[ $instance_type == "r3.8xlarge" ]]; then
     mkdir /mnt2
+    # To turn TRIM support on, uncomment the following line.
     #echo '/dev/sdc /mnt2  ext4  defaults,noatime,nodiratime,discard 0 0' >> /etc/fstab
     mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/sdc
     mount -o $EXT4_MOUNT_OPTS /dev/sdc /mnt2
