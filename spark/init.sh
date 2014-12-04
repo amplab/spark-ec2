fcd383a5f6aa5f458ee1d879c978c80ade4308c1
andrewor14 <andrew@databricks.com>
2014-12-04 13:32:40 -0800

    Support Spark 1.1.1

--- spark/init.sh
+++ spark/init.sh
@@ -95,6 +95,13 @@ else
         wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.0-bin-cdh4.tgz
       fi
       ;;
+    1.1.1)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.1-bin-hadoop1.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.1-bin-cdh4.tgz
+      fi
+      ;;
     *)
       echo "ERROR: Unknown Spark version"
       return
