a05287a98f0218b041052b80a83a7febebd3158d
Fred Cons <cons@fullsix.com>
2015-01-05 11:58:38 +0100

    Symlinking ganglia work dir to /mnt

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -4,10 +4,6 @@
 rm -rf /var/lib/ganglia/rrds/*
 rm -rf /mnt/ganglia/rrds/*
 
-# Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
-rmdir /var/lib/ganglia/rrds
-ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
-
 # Make sure rrd storage directory has right permissions
 mkdir -p /mnt/ganglia/rrds
 chown -R nobody:nobody /mnt/ganglia/rrds
@@ -24,3 +20,7 @@ for node in $SLAVES $OTHER_MASTERS; do
   ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
 done
 wait
+
+# Post-package installation : Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
+rmdir /var/lib/ganglia/rrds
+ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
