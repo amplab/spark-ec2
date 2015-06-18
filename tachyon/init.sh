a8d38eebb0330232363cb45f6205c8681d853657
cc <myairia@gmail.com>
2015-06-18 13:11:12 +0800

    add tachyon releases 0.6.4

--- tachyon/init.sh
+++ tachyon/init.sh
@@ -38,6 +38,15 @@ else
         wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.0-cdh4-bin.tar.gz
       fi
       ;;
+    0.6.4)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.4-bin.tar.gz
+      elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.4-cdh4-bin.tar.gz
+      else
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.4-hadoop2.4-bin.tar.gz
+      fi
+      ;;
     *)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
         wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-bin.tar.gz
