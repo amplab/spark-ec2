ea481667e8293ea24a8b887f70e153e45241cf0c
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2015-07-03 11:29:19 +0200

    added .Rprofile

--- rstudio/init.sh
+++ rstudio/init.sh
@@ -17,3 +17,28 @@ adduser rstudio
 # maybe have the user set the password themselves instead of this? 
 # otherwise this might be a security issue. 
 echo 'rstudio' | passwd  --stdin rstudio
+
+# create an .Rprofile that automatically connects to Spark 
+echo ".First <- function(){ 
+	cat('Now connecting to Spark for you.') 
+	region_ip <- system('curl http://169.254.169.254/latest/meta-data/public-hostname', intern=TRUE) 
+ 
+	spark_link <- paste0('spark://', region_ip, ':7077') 
+ 
+	.libPaths(c(.libPaths(), '/root/spark/R/lib')) 
+	Sys.setenv(SPARK_HOME = '/root/spark') 
+	Sys.setenv(PATH = paste(Sys.getenv(c('PATH')), '/root/spark/bin', sep=':')) 
+	library(SparkR) 
+ 
+	sc <- sparkR.init(spark_link) 
+	sqlContext <- sparkRSQL.init(sc) 
+
+	cat('      ____              __ \\n')
+	cat('     / __/__  ___ _____/ /__ \\n')
+	cat('    _\\\\ \\\\/ _ \\\\/ _ \`/ __/   _/ \\n')
+	cat('   /__ / .__/\\\\_,_/_/ /_/\\\\_\\   version 1.4.0 \\n')
+	cat('      /_/ \\n')
+	cat(' \\n')
+	cat('Spark Context available as \"sc\". \\n')
+	cat('Spark SQL Context available as \"sqlContext\". \\n')
+}"  > /root/.Rprofile 
