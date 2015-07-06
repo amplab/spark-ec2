26b87677ed4dce7989c7e9a0a36bccac15f05004
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2015-07-06 13:14:23 +0200

    turned rprofile into a starter script

--- rstudio/init.sh
+++ rstudio/init.sh
@@ -1,9 +1,3 @@
-# we need to add correct epel 
-# http://stackoverflow.com/questions/31180061/r-3-2-on-aws-ami
-# yum update -y --enablerepo=epel --skip-broken
-# yum upgrade -y --enablerepo=epel --skip-broken
-# yum install R --enablerepo=epel --skip-broken
-
 # download rstudio 
 wget http://download2.rstudio.org/rstudio-server-rhel-0.99.446-x86_64.rpm
 sudo yum install --nogpgcheck -y rstudio-server-rhel-0.99.446-x86_64.rpm
@@ -18,29 +12,23 @@ adduser rstudio
 # otherwise this might be a security issue. 
 echo 'rstudio' | passwd  --stdin rstudio
 
-# create an .Rprofile that automatically connects to Spark 
-# echo ".First <- function(){ 
-# 	cat('Now connecting to Spark for you.') 
-# 	region_ip <- system('curl http://169.254.169.254/latest/meta-data/public-hostname', intern=TRUE) 
- 
-# 	spark_link <- paste0('spark://', region_ip, ':7077') 
- 
-# 	.libPaths(c(.libPaths(), '/root/spark/R/lib')) 
-# 	Sys.setenv(SPARK_HOME = '/root/spark') 
-# 	Sys.setenv(PATH = paste(Sys.getenv(c('PATH')), '/root/spark/bin', sep=':')) 
-# 	library(SparkR) 
- 
-# 	sc <- sparkR.init(spark_link) 
-# 	sqlContext <- sparkRSQL.init(sc) 
+sudo chmod a+w /mnt/spark
+sudo chmod a+w /mnt/spark2
 
-# 	cat('      ____              __ \\n')
-# 	cat('     / __/__  ___ _____/ /__ \\n')
-# 	cat('    _\\\\ \\\\/ _ \\\\/ _ \`/ __/   _/ \\n')
-# 	cat('   /__ / .__/\\\\_,_/_/ /_/\\\\_\\   version 1.4.0 \\n')
-# 	cat('      /_/ \\n')
-# 	cat(' \\n')
-# 	cat('Spark Context available as \"sc\". \\n')
-# 	cat('Spark SQL Context available as \"sqlContext\". \\n')
-# }"  > /home/rstudio/.Rprofile 
+# create a Rscript that connects to Spark, to help starting user
+echo "cat('Now connecting to Spark for you.') 
+	region_ip <- system('curl http://169.254.169.254/latest/meta-data/public-hostname', intern=TRUE) 
+ 
+	spark_link <- paste0('spark://', region_ip, ':7077') 
+ 
+	.libPaths(c(.libPaths(), '/root/spark/R/lib')) 
+	Sys.setenv(SPARK_HOME = '/root/spark') 
+	Sys.setenv(PATH = paste(Sys.getenv(c('PATH')), '/root/spark/bin', sep=':')) 
+	library(SparkR) 
+ 
+	sc <- sparkR.init(spark_link) 
+	sqlContext <- sparkRSQL.init(sc) 
 
-# asdfas
+	cat('Spark Context available as \"sc\". \\n')
+	cat('Spark SQL Context available as \"sqlContext\". \\n')
+"  > /home/rstudio/startSpark.R
