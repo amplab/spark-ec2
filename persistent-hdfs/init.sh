18047a50dfe3999263d8c7a8e8372b5c0a41d859
Daniel Darabos <darabos.daniel@gmail.com>
2015-07-15 15:00:29 +0200

    Return positive value on error in persistent-hdfs/init.sh

--- persistent-hdfs/init.sh
+++ persistent-hdfs/init.sh
@@ -40,7 +40,7 @@ case "$HADOOP_MAJOR_VERSION" in
 
   *)
      echo "ERROR: Unknown Hadoop version"
-     return -1
+     return 1
 esac
 cp /root/hadoop-native/* /root/persistent-hdfs/lib/native/
 /root/spark-ec2/copy-dir /root/persistent-hdfs
