ef5e81524baa60de1e7552c908a0021d2ea79213
Manu Bansal <manub@stanford.edu>
2016-09-02 13:20:30 -0700

    adding git install to ubuntu setup

--- setup.sh
+++ setup.sh
@@ -14,12 +14,13 @@ elif [[ $DISTRIB_ID = "Ubuntu" ]]; then
   sudo apt-get install -y -q pssh
 
   #also install java on the first master since stock ubuntu ami does not
-  #come with java pre-installed
+  #come with java pre-installed; also install git
   sudo apt-get install -y -q openjdk-7-jdk
   if [[ "x$JAVA_HOME" == "x" ]]; then
     sudo sh -c 'echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /etc/environment'
     source /etc/environment
   fi
+  sudo apt-get install -y -q git
 
 fi
 
