46b7b3988586e8d394c2c51f9e3a734c9821e661
Manu Bansal <manub@stanford.edu>
2016-09-02 15:05:27 -0700

    master apt update only on first time java install

--- setup.sh
+++ setup.sh
@@ -13,11 +13,11 @@ if [[ $DISTRIB_ID = "Centos" ]]; then
 elif [[ $DISTRIB_ID = "Ubuntu" ]]; then
   sudo apt-get install -y -q pssh
 
-  #also install java on the first master since stock ubuntu ami does not
-  #come with java pre-installed; also install git
-  sudo apt-get update
-  sudo apt-get install -y -q openjdk-7-jdk
   if [[ "x$JAVA_HOME" == "x" ]]; then
+    #also install java on the first master since stock ubuntu ami does not
+    #come with java pre-installed; also install git
+    sudo apt-get update
+    sudo apt-get install -y -q openjdk-7-jdk
     sudo sh -c 'echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /etc/environment'
     source /etc/environment
   fi
