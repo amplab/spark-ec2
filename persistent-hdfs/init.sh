0e5229b1b33a7704305a6614492a9955cffb388e
Manu Bansal <manub@stanford.edu>
2016-09-02 00:53:03 -0700

    more master module init for ubuntu

--- persistent-hdfs/init.sh
+++ persistent-hdfs/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd /root > /dev/null
+pushd ~ > /dev/null
 
 if [ -d "persistent-hdfs" ]; then
   echo "Persistent HDFS seems to be installed. Exiting."
@@ -14,7 +14,7 @@ case "$HADOOP_MAJOR_VERSION" in
     tar xvzf hadoop-1.0.4.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
     mv hadoop-1.0.4/ persistent-hdfs/
-    cp /root/hadoop-native/* /root/persistent-hdfs/lib/native/
+    cp ~/hadoop-native/* ~/persistent-hdfs/lib/native/
     ;;
   2)
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz
@@ -24,9 +24,9 @@ case "$HADOOP_MAJOR_VERSION" in
     mv hadoop-2.0.0-cdh4.2.0/ persistent-hdfs/
 
     # Have single conf dir
-    rm -rf /root/persistent-hdfs/etc/hadoop/
-    ln -s /root/persistent-hdfs/conf /root/persistent-hdfs/etc/hadoop
-    cp /root/hadoop-native/* /root/persistent-hdfs/lib/native/
+    rm -rf ~/persistent-hdfs/etc/hadoop/
+    ln -s ~/persistent-hdfs/conf ~/persistent-hdfs/etc/hadoop
+    cp ~/hadoop-native/* ~/persistent-hdfs/lib/native/
     ;;
   yarn)
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.4.0.tar.gz
@@ -36,14 +36,14 @@ case "$HADOOP_MAJOR_VERSION" in
     mv hadoop-2.4.0/ persistent-hdfs/
 
     # Have single conf dir
-    rm -rf /root/persistent-hdfs/etc/hadoop/
-    ln -s /root/persistent-hdfs/conf /root/persistent-hdfs/etc/hadoop
+    rm -rf ~/persistent-hdfs/etc/hadoop/
+    ln -s ~/persistent-hdfs/conf ~/persistent-hdfs/etc/hadoop
     ;;
 
   *)
      echo "ERROR: Unknown Hadoop version"
      return 1
 esac
-/root/spark-ec2/copy-dir /root/persistent-hdfs
+~/spark-ec2/copy-dir ~/persistent-hdfs
 
 popd > /dev/null
--- spark/init.sh
+++ spark/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd /root > /dev/null
+pushd ~ > /dev/null
 
 if [ -d "spark" ]; then
   echo "Spark seems to be installed. Exiting."
