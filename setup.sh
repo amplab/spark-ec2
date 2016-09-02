f3f4731541b4fdb436e7c415eca0faa8171c70d9
Manu Bansal <manub@stanford.edu>
2016-09-02 13:54:44 -0700

    update apt before installing java

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ elif [[ $DISTRIB_ID = "Ubuntu" ]]; then
 
   #also install java on the first master since stock ubuntu ami does not
   #come with java pre-installed; also install git
+  sudo apt-get update
   sudo apt-get install -y -q openjdk-7-jdk
   if [[ "x$JAVA_HOME" == "x" ]]; then
     sudo sh -c 'echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /etc/environment'
