0817e4beec486a2e8ca934df56e55135edd8e0f6
Manu Bansal <manub@stanford.edu>
2016-09-01 22:13:55 -0700

    slave setup log dirs

--- setup.sh
+++ setup.sh
@@ -87,11 +87,15 @@ if [[ DISTRIB_ID = "Centos" ]]; then
     --timeout 0 \
     "spark-ec2/setup-slave.sh"
 elif [[ DISTRIB_ID = "Ubuntu" ]]; then
+  mkdir slave-setup-stdout
+  mkdir slave-setup-stderr
   parallel-ssh --inline \
     --host "$MASTERS $SLAVES" \
     --user $USER \
     --extra-args "-t -t $SSH_OPTS" \
     --timeout 0 \
+    --outdir slave-setup-stdout \
+    --errdir slave-setup-stderr \
     "spark-ec2/setup-slave.sh"
 fi
 
