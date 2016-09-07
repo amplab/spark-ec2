c760e15c95b9bb7f8c7a1a55e4915f854a7024d8
Manu Bansal <manub@stanford.edu>
2016-09-07 11:10:04 -0700

    taking out ganglia setup on ubuntu

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -7,20 +7,20 @@ DISTRIB_ID=Centos
 if [[ -e /etc/lsb-release ]]; then source /etc/lsb-release; fi
 echo "DISTRIB_ID=$DISTRIB_ID"
 
-
-# NOTE: Remove all rrds which might be around from an earlier run
-sudo rm -rf /var/lib/ganglia/rrds/*
-sudo rm -rf /mnt/ganglia/rrds/*
-
-# Make sure rrd storage directory has right permissions
-mkdir -p /mnt/ganglia/rrds
-chown -R nobody:nobody /mnt/ganglia/rrds
-
-# Install ganglia
-# TODO: Remove this once the AMI has ganglia by default
-
-
 if [[ $DISTRIB_ID = "Centos" ]]; then
+
+  # NOTE: Remove all rrds which might be around from an earlier run
+  sudo rm -rf /var/lib/ganglia/rrds/*
+  sudo rm -rf /mnt/ganglia/rrds/*
+
+  # Make sure rrd storage directory has right permissions
+  mkdir -p /mnt/ganglia/rrds
+  chown -R nobody:nobody /mnt/ganglia/rrds
+
+  # Install ganglia
+  # TODO: Remove this once the AMI has ganglia by default
+
+
   GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
   if ! rpm --quiet -q $GANGLIA_PACKAGES; then
     yum install -q -y $GANGLIA_PACKAGES;
@@ -29,6 +29,10 @@ if [[ $DISTRIB_ID = "Centos" ]]; then
     ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
   done
   wait
+
+  # Post-package installation : Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
+  if [[ -d /var/lib/ganglia/rrds ]]; then sudo rmdir /var/lib/ganglia/rrds; fi
+  sudo ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
 
 elif [[ $DISTRIB_ID = "Ubuntu" ]]; then
   echo "WARNING: Skipping ganglia on ubuntu..."
@@ -39,8 +43,8 @@ elif [[ $DISTRIB_ID = "Ubuntu" ]]; then
   #done
   #wait
 
-	# Post-package installation : Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
-	if [[ -d /var/lib/ganglia/rrds ]]; then sudo rmdir /var/lib/ganglia/rrds; fi
-	sudo ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
+  ## Post-package installation : Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
+  #if [[ -d /var/lib/ganglia/rrds ]]; then sudo rmdir /var/lib/ganglia/rrds; fi
+  #sudo ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
 
 fi
