4e1a916eec8da62089f3ec076a27a7a31affeaf2
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2017-06-21 14:09:19 +0300

    Use Hadoop 2.7 for newer Spark versions with yarn.

--- spark/init.sh
+++ spark/init.sh
@@ -130,7 +130,7 @@ else
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
         wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-cdh4.tgz
       else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.7.tgz
       fi
       if [ $? != 0 ]; then
         echo "ERROR: Unknown Spark version"
